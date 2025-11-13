4f325fcf6ab59048ec2dbd31e054c4db8c045b5b
qinmoM <ma200511@qq.com>
2025-11-13 21:23:56 +0800

    Test for jsonHandle class : write when no file has been create.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using json = nlohmann::json;
 
 int main()
 {
+    // test&
     json data;
     data["name"] = "qinmo";
     std::cout << data["name"] << std::endl;
@@ -13,8 +14,13 @@ int main()
     jsonfile.write(data);
     nlohmann::json jsonData = jsonfile.read();
     std::cout << jsonData << std::endl;
+
     jsonHandle handle;
     nlohmann::json temp = handle.read("../data/test.json");
     std::cout << temp << std::endl;
+
+    temp.clear();
+    handle.write("../data/test1.json", data);
+    temp = handle.read("../data/test1.json");
     return 0;
 }
